070bdfa3e1e9578362adb3facf5ac1e618e050ff
Purify12 <marchelsitumorang2000@gmail.com>
2021-12-24 21:07:47 +0700

    make it better

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb+srv://jose:Josepoenya1@cluster0.1ug8h.mongodb.net/projectbackend13?authSource=admin&replicaSet=atlas-14383t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+    db: 'mongodb+srv://jose:Josepoenya1@cluster0.1ug8h.mongodb.net/projectbackend13?authSource=admin&replicaSet=atlas-14383t-shard-0&readPreference=primary&projectbackend13=MongoDB%20Compass&ssl=true'
 }
